389fbd24948f64d9b002fa9c7928f3cb68e4210b
Tushar-7742 <parihartushar4@gmail.com>
2025-09-01 20:35:17 +0530

    Updated website with new changes

--- app/page.tsx
+++ app/page.tsx
@@ -332,7 +332,7 @@ const skills = [
     const certificates = [
     {
       name: "Blender Essential: From Beginner to 3D Masterclass",
-      image: "/certificates/blender.jpg",
+      image: "/certificates/blender 4.jpg",
       color: "from-orange-400 to-blue-400",
       issuer: "Udemy - Learnify IT",
       year: "2024",
@@ -343,7 +343,7 @@ const skills = [
     },
     {
       name: "UI/UX Design Masterclass with Adobe XD",
-      image:"/certificates/ui ux.jpg",
+      image:"/certificates/ui ux 6.jpg",
       color: "from-blue-400 to-indigo-400",
       issuer: "Udemy - James Joab Soren",
       year: "2024",
@@ -354,7 +354,7 @@ const skills = [
     },
     {
       name: "Professional Diploma in Social Media Marketing",
-      image: "/certificates/social Media Marketing.jpg",
+      image: "/certificates/social Media Marketing 5.jpg",
       color: "from-pink-400 to-purple-400",
       issuer: "Udemy - MTF Institute",
       year: "2025",
@@ -365,7 +365,7 @@ const skills = [
     },
     {
       name: "AI-Powered Graphic Design: Mastering Figma",
-      image: "/certificates/AI.jpg",
+      image: "/certificates/AI 3.jpg",
       color: "from-purple-400 to-pink-400",
       issuer: "Udemy - Skool of AI",
       year: "2024",
@@ -376,7 +376,7 @@ const skills = [
     },
     {
       name: "Master Adobe Illustrator: Design Awesome Logos",
-      image: "/certificates/Adobe Illustrator.jpg",
+      image: "/certificates/Adobe Illustrator 1.jpg",
       color: "from-orange-400 to-pink-400",
       issuer: "Udemy - Jawid Firooz",
       year: "2024",
@@ -387,7 +387,7 @@ const skills = [
     },
     {
       name: "Essential Skills in Adobe Photoshop 2024",
-      image: "/certificates/Adobe Photoshop.jpg",
+      image: "/certificates/Adobe Photoshop 2.jpg",
       color: "from-blue-400 to-cyan-400",
       issuer: "LinkedIn Learning - Adobe",
       year: "2025",
